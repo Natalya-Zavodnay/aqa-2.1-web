69f01455f4e892ea96f1faa452224b3ea2818ded
Natalya-Zavodnay <nz41330@gmail.com>
2021-07-07 10:35:48 +0300

    add tests

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,6 +1,7 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -8,10 +9,13 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class CallbackTest {
-    @Test
 
+    @BeforeEach
+    void setUp() {open("http://localhost:9999");
+    }
+
+    @Test
     void getTrueInputValidForm() {
-        open("http://localhost:9999");
         SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
         form.$("[type=text]").setValue("Петров Сергей");
         form.$("[type=tel]").setValue("+79999999999");
@@ -22,7 +26,6 @@ class CallbackTest {
 
     @Test
     void errorExpectedWhenEmptyFieldName() {
-        open("http://localhost:9999");
         $("[type='text']").setValue("");
         $("[type='tel']").setValue("+79999999999");
         $("[class='checkbox__box']").click();
@@ -33,7 +36,6 @@ class CallbackTest {
 
     @Test
     void errorExpectedWhenInputIncorrectName() {
-        open("http://localhost:9999");
         $("[type='text']").setValue("Niki");
         $("[type='tel']").setValue("+79999999999");
         $("[class='checkbox__box']").click();
@@ -44,13 +46,30 @@ class CallbackTest {
 
     @Test
     void errorExpectedWhenInputIncorrectTelNumber() {
-        open("http://localhost:9999");
         $("[type='text']").setValue("Петров Сергей");
         $("[type='tel']").setValue("+7 (999)-999-99-99");
         $("[class='checkbox__box']").click();
         $("[type='button']").click();
         $("[data-test-id=phone].input_invalid .input__sub").shouldHave
                 (exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+    @Test
+    void errorExpectedWhenEmptyFieldPhone() {
+        $("[type='text']").setValue("Петров Сергей");
+        $("[type='tel']").setValue("");
+        $("[class='checkbox__box']").click();
+        $("[type='button']").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave
+                (exactText("Поле обязательно для заполнения"));
+    }
+    @Test
+    void errorExpectedWhenUncheckedCheckbox() {
+        $("[type='text']").setValue("Петров Сергей");
+        $("[type='tel']").setValue("+79999999999");
+        $("[type='button']").click();
+        $("[data-test-id=agreement].input_invalid").shouldHave
+                (exactText("Я соглашаюсь с условиями обработки и использования моих " +
+                        "персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 
 }
